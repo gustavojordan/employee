8c1a151961827a6fb5181d4db6350ac966cd65a7
Gustavo Jordan <505354+gustavojordan@users.noreply.github.com>
2022-02-14 05:17:56 -0500

    Add new tests

--- tests/Api/EmployeeTestCest.php
+++ tests/Api/EmployeeTestCest.php
@@ -56,7 +56,6 @@ class EmployeeTestCest
 
     public function createEmployeeWrongParameters(ApiTester $I)
     {
-
         $I->wantToTest('create employee without name should return 422');
 
         $user = User::find(1);
@@ -67,11 +66,29 @@ class EmployeeTestCest
         $I->seeResponseCodeIs(422);
     }
 
+    public function createEmployeeInvalidSin(ApiTester $I)
+    {
+        $I->wantToTest('create employee invalid sin should return 422');
+
+        $user = User::find(1);
+        $token = JWTAuth::fromUser($user);
+        $I->amBearerAuthenticated($token);
+
+        $employee = $this->employee;
+        $employee['sin'] = '123 456 789';
+
+        $I->sendPOST('employee', $employee);
+
+        $I->seeResponseCodeIs(422);
+    }
+
     public function deleteEmployee(ApiTester $I)
     {
         $I->wantToTest('delete employee');
+
         $user = User::find(1);
         $token = JWTAuth::fromUser($user);
+
         $I->amBearerAuthenticated($token);
         $I->sendDELETE('employee/1');
         $I->seeResponseCodeIs(200);
@@ -86,5 +103,70 @@ class EmployeeTestCest
         $I->sendPUT('employee/1', ['first_name' => 'NameChange']);
         $I->seeResponseCodeIs(202);
         $I->seeResponseContainsJson(['first_name' => 'NameChange']);
+    }
+
+    public function clockIn(ApiTester $I)
+    {
+        $I->wantToTest('clock in employee one');
+        $user = User::find(1);
+        $token = JWTAuth::fromUser($user);
+        $I->amBearerAuthenticated($token);
+        $I->sendPOST('employee/1/clock/in');
+        $I->seeResponseCodeIs(201);
+    }
+
+    public function clockInAfterClockedBefore(ApiTester $I)
+    {
+        $I->wantToTest('clock in after clocked before');
+        $user = User::find(1);
+        $token = JWTAuth::fromUser($user);
+        $I->amBearerAuthenticated($token);
+        $I->sendPOST('employee/1/clock/in');
+        $I->seeResponseCodeIs(201);
+
+        $I->sendPOST('employee/1/clock/in');
+        $I->seeResponseCodeIs(422);
+    }
+
+    public function clockedOut(ApiTester $I)
+    {
+        $I->wantToTest('clock out employee one');
+        $user = User::find(1);
+        $token = JWTAuth::fromUser($user);
+        $I->amBearerAuthenticated($token);
+
+        $I->sendPOST('employee/1/clock/in');
+        $I->seeResponseCodeIs(201);
+
+        $I->sendPOST('employee/1/clock/out');
+        $I->seeResponseCodeIs(201);
+    }
+
+    public function clockOutAfterClockedOut(ApiTester $I)
+    {
+        $I->wantToTest('clock out after clocked out');
+        $user = User::find(1);
+        $token = JWTAuth::fromUser($user);
+        $I->amBearerAuthenticated($token);
+
+        $I->sendPOST('employee/1/clock/in');
+        $I->seeResponseCodeIs(201);
+
+        $I->sendPOST('employee/1/clock/out');
+        $I->seeResponseCodeIs(201);
+
+        $I->sendPOST('employee/1/clock/out');
+        $I->seeResponseCodeIs(422);
+    }
+
+    public function clockOutWithoutClockedIn(ApiTester $I)
+    {
+        $I->wantToTest('clock out without clocked in');
+        $user = User::find(1);
+        $token = JWTAuth::fromUser($user);
+        $I->amBearerAuthenticated($token);
+
+        $I->sendPOST('employee/1/clock/out');
+        $I->seeResponseCodeIs(422);
     }
 }
